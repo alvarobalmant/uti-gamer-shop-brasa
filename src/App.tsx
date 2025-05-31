010fb66505d5fc5d8dd49fc065b3c95057b83e13
alvaro <balmantalvaro@gmail.com>
2025-05-31 14:14:49 -0300

    Fix: Corrige restauração da posição de scroll ao voltar página

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { Toaster } from "@/components/ui/toaster";
-import { Toaster as SonnerToaster } from "@/components/ui/sonner";
+import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate, ScrollRestoration } from "react-router-dom"; // Import ScrollRestoration
+// Import ScrollRestoration
+import { BrowserRouter, Routes, Route, Navigate, ScrollRestoration } from "react-router-dom"; 
 import { AuthProvider, useAuth } from "@/hooks/useAuth";
 import { CartProvider } from "@/contexts/CartContext";
 import Index from "./pages/Index";
@@ -41,7 +42,8 @@ const App = () => (
           <Toaster />
           <Sonner />
           <BrowserRouter>
-            <ScrollRestoration /> {/* Add ScrollRestoration here */}
+            {/* Add ScrollRestoration here */}
+            <ScrollRestoration /> 
             <Routes>
               {/* Public Routes */}
               <Route path="/" element={<Index />} />
